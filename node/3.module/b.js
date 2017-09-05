a8583918e430f7863067ea682ec934c7a968c8e2
JYB7913 <702674512@qq.com>
2017-09-05 13:44:27 +0800

    three

--- node/3.module/b.js
+++ node/3.module/b.js
@@ -1,9 +1,9 @@
 var b = 1;
 global.b = b;
 /*
-* (function(exports,require,module,__filename,__dirname){
-*   module.exports = exports = {}
-*   var b = 1;
-*   return module.exports
-* })()
-* */
+ * (function(exports,require,module,__filename,__dirname){
+ *   module.exports = exports = {}
+ *   var b = 1;
+ *   return module.exports
+ * })()
+ * */

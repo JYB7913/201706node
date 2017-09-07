00892d391bd8a2729b19a937672fe93647ffa7b0
zhufengzhufeng <894918097@qq.com>
2017-09-07 12:03:08 +0800

    解析less css

--- vue/sixthday/node.md
+++ vue/sixthday/node.md
@@ -12,5 +12,26 @@ scripts:{
     "build":"webpack"
 }
 ```
+## 解析es6语法
+- babel-core是babel的核心包,使用babel必须安装
+- babel-loader 是babel的翻译官,用来翻译语法的,但是他不懂
+- 配置预设让他懂es6 babel-preset-es2015
+```
+npm install babel-core babel-loader babel-preset-es2015 --save-dev
+```
 
+## 解析es7语法
+- es2015 stage-3 stage-2 stage-1 stage-0(es7语法)
+```
+npm install babel-preset-stage-0 --save-dev
+```
 
+## 解析css 将css看成一个模块
+```
+npm install css-loader style-loader --save-dev
+```
+
+## 解析less
+```
+npm install less less-loader --save-dev
+```
--- vue/sixthday/webpack.config.js
+++ vue/sixthday/webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
     output:{
         filename:'bundle.js', //打包后的文件名
         path:path.resolve('./dist'), //path必须是绝对路径
+    },
+    module:{ //模块
+        rules:[ //给模块配置规则
+            //匹配js 使用babel-loader但是不管node_modules文件,如果用到babel需要配置一个文件.babelrc 会自动查找其中预设进行解析
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/},
+            //将css文件匹配到先解析成css 再将css插入到style便签内,写法:从右往左写
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+        ]
     }
 };

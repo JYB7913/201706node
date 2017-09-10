238f41fde93aa886050e6715e4c80c10c98501b7
zhufengzhufeng <894918097@qq.com>
2017-09-10 13:05:06 +0800

    server-route

--- node/12.http/1.server.js
+++ node/12.http/1.server.js
@@ -4,6 +4,7 @@ let listener = (req,res) =>{
     //res代表的是服务器
     //res是可写流，有write和end
     //res.write('你好');//如果不调用end 则状态一直是pending;
+    res.setHeader('Content-Type','text/plain;charset=utf-8');
     res.end('你好'); //中文会乱码
 };
 //创建一个服务，放入一个监听函数
